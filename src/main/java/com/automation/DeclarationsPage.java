09c0bd24602d0b5b5af7e6571c2cda650c92b74a
mohan-vasanth <amohan@opalminds.com>
2026-05-14 09:27:58 +0530

    ipt

--- src/main/java/com/automation/DeclarationsPage.java
+++ src/main/java/com/automation/DeclarationsPage.java
@@ -65,6 +65,7 @@ public class DeclarationsPage {
             button.click();
             return;
         }
+        
 
         Boolean clicked = (Boolean) page.evaluate("""
                 () => {
